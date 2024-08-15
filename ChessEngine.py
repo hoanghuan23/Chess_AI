9cb89781de9bb2db15e1cb753c71023d8a8fa556
hoanghuan23 <huanhoang2003@gmail.com>
2024-08-15 23:39:38 +0700

    update

--- ChessEngine.py
+++ ChessEngine.py
@@ -25,8 +25,23 @@ class Game_state():
 
 
 class Move():
+    # gán biến cho các hàng của bàn cờ
+    ranksTorows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksTorows.items()}
+    # gán biến cho các cột của bàn cờ
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
     def __init__(self, startSq, endSq, board):
-        self.startSq = startSq[0]
-        self.startSq = startSq[1]
-        self.endSq = endSq[0]
-        self.endSq = endSq[1]
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]  # quân cờ được di chuyển
+        self.pieceCaptured = board[self.endRow][self.endCol]  # quân cờ bị bắt (nếu có)
+
+    def getChessNotation(self):  # chuyển đổi 2 vị trí là vị trí trước khi và sau khi di chuyển
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+
+    def getRankFile(self, row, col):  # chuyển đổi một cặp hàng thành ký hiệu cờ vua (ví dụ như 6,4 thành e,2)
+        return self.colsToFiles[col] + self.rowsToRanks[row]
