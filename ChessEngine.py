901c9ce8108f3cd17fb03befe54f881819d22ec2
hoanghuan23 <huanhoang2003@gmail.com>
2024-09-03 22:05:10 +0700

    hàm di chuyển quân hậu

--- ChessEngine.py
+++ ChessEngine.py
@@ -137,7 +137,7 @@ class Game_state:
                     break
                 i = i + 1
 
-    def getQueenMoves(self, row, col, moves):
+    def getQueenMoves(self, row, col, moves):  # di chuyển quân hậu
         directions = [(-1, -1), (0, -1), (1, -1), (1, 0), (1,1), (0, 1), (-1, 1), (-1, 0)]
         enemycolor = 'b' if self.whiteToMove else 'w'
         for d in directions:
